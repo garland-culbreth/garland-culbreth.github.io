86ea61b848e84f475f0f6abb1d4a7e47448f6b15
Garland Culbreth <70354045+garland-culbreth@users.noreply.github.com>
2026-03-20 00:29:13 -0700

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
     remarkPlugins: [remarkToc, [remarkCollapse, { test: "Table of contents" }]],
     shikiConfig: {
       // For more themes, visit https://shiki.style/themes
-      themes: { light: "ayu-light", dark: "ayu-mirage" },
+      themes: { light: "min-light", dark: "night-owl" },
       defaultColor: false,
       wrap: false,
       transformers: [
